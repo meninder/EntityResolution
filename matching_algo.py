3f5d5204eaa01f287519d98caaecc65e7f795787
apo <tagliabue.jacopo@gmail.com>
2022-01-17 09:59:41 -0500

    ciao!

--- matching_algo.py
+++ matching_algo.py
@@ -1,8 +1,10 @@
+from typing import Dict
 from dynamo_utils import get_all_keys, get_key_probability_and_name
 from jellyfish import jaro_similarity
 from string_cleaning_utils import clean_org
-
 import logging
+from data_utils import MatchType
+from data_utils import ScoredKey, Ticker, Entity, PMatch
 
 
 logger = logging.getLogger(__name__)
@@ -12,112 +14,123 @@ logger.addHandler(streamHandler)
 
 
 def calculate_probability_ee(e1, e2):
+    """
+    Comment here etc.
+    """
     p_ee = jaro_similarity(e1, e2)
     return p_ee
 
 
-def get_largest_candidate_key(e1, e2, keys):
-    scores_e1 = [(k, jaro_similarity(e1, k)) for k in keys]
-    scores_e1.sort(key=lambda x: x[1], reverse=True)
-    key_score_tuple_e1 = scores_e1[0]
-
-    scores_e2 = [(k, jaro_similarity(e2, k)) for k in keys]
-    scores_e2.sort(key=lambda x: x[1], reverse=True)
-    key_score_tuple_e2 = scores_e2[0]
-
-    return key_score_tuple_e1, key_score_tuple_e2
-
-
-def get_candidate_company_probability(key_score_tuple_e1, key_score_tuple_e2):
-    key1, prob_ec1 = key_score_tuple_e1
-    key2, prob_ec2 = key_score_tuple_e2
-
-    ticker_probability_tuple1 = get_key_probability_and_name(key1)
-    ticker_probability_tuple2 = get_key_probability_and_name(key2)
-
-    return ticker_probability_tuple1, ticker_probability_tuple2
+# not sure if name is correct, but just breaking down the other function
+# now the actual function is more modular and can be easilty tested with unit tests etc
+def score_candidates(candidates: list[str], keys: list) -> list[ScoredKey]:
+    """
+    Comment here etc.
+    """
+    # this is lazy, as I'm returning a list following the same candidate order
+    # the best thing wuold be however to build a small class with source and target, and make explicit
+    # the link between candidates and their score
+    # here, I'm doing a first pass in making it modular
+    return [ get_largest_candidate_key(c, keys) for c in candidates ]
 
 
-def calculate_probability_match(e1, e2):
+def get_largest_candidate_key(candidate: str, keys: list) -> ScoredKey:
+    """
+    Comment here etc.
+    """
+    clean_candidate = clean_org(candidate) # not sure about this here, but for now it's ok
+    scores = [(k, jaro_similarity(clean_candidate, k)) for k in keys]
+    scores.sort(key=lambda x: x[1], reverse=True)
 
-    theta_ee = 0.75
-    theta_l = 0.65
+    return ScoredKey(scores[0], scores[1])
 
-    dct_out = {}
-    dct_out['probability'] = 0.0
-    dct_out['match_case'] = None
-    dct_out[e1] = {}
-    dct_out[e2] = {}
 
-    e1_clean = clean_org(e1)
-    e2_clean = clean_org(e2)
+# same trick as above
+def get_company_probabilities(candidates: list[str]) -> list[Ticker]:
+    """
+    Comment here etc.
+    """
+    return [get_candidate_company_probability(c) for c in candidates]
 
-    logger.info('Getting full candidate list from Dynamo')
+
+# we just need the names as input here
+def get_candidate_company_probability(candidate: str):
+    """
+    Comment here etc.
+    """
+    ticker_probability = get_key_probability_and_name(candidate)
+
+    return Ticker(ticker=ticker_probability[0], probability=ticker_probability[1])
+
+
+# I would type all input in all function if not super painful
+# it really improves code a lot
+def calculate_probability_match(e1: str, e2: str, theta_ee: str = 0.75) -> PMatch:
+    """
+    
+    Comments here, as in the main. Brief function description 
+    and params description, for example following google style:
+
+    https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+
+    Note: I'm removing some logging to focus on logic, not because logging is bad
+
+    """
+
+    # theta_ee = 0.75 # commenting it for now, pass it from outside with a default, to make function testable
+    # theta_l = 0.65  # is this used at all? Commenting it for now
+
+    # this is a call to a db, so probably should check for error
     keys = get_all_keys()
-    logger.info(f'There are {len(keys)} keys')
-
+    entities = [e1, e2]
     # get k1, k2 (candidate synomous with key)
-    key_score_tuple_e1, key_score_tuple_e2 = get_largest_candidate_key(e1_clean, e2_clean, keys)
-    logger.info(f'{e1} is mapped to key {key_score_tuple_e1[0]} with probability {key_score_tuple_e1[1]}')
-    logger.info(f'{e2} is mapped to key {key_score_tuple_e2[0]} with probability {key_score_tuple_e2[1]}')
+    # probably terrible variable name
+    candidates_best_scores = score_candidates(entities, keys)
 
-    dct_out[e1]['key'] = key_score_tuple_e1[0]
-    dct_out[e2]['key'] = key_score_tuple_e2[0]
-    dct_out[e1]['key_probability'] = key_score_tuple_e1[1]
-    dct_out[e2]['key_probability'] = key_score_tuple_e2[1]
-    dct_out[e1]['ticker'] = 'None'
-    dct_out[e2]['ticker'] = 'None'
-    dct_out[e1]['ticker_probability'] = 0
-    dct_out[e2]['ticker_probability'] = 0
+    # if it's a perfect match (if I get it, hope I'm reading it right)
+    if candidates_best_scores[0].key == candidates_best_scores[1].key:
+        # return here and avoid one level of nesting
+        return PMatch(
+            probability=min(candidates_best_scores[0].score, candidates_best_scores[1].score),
+            match_case=MatchType.PERFECT,
+            source=Entity(scores=candidates_best_scores[0], ticker=None),
+            target=Entity(scores=candidates_best_scores[1], ticker=None)
+            )
+    # if not, let's proceed with the other cases
+    
+    # get tickers corresponding to keys
+    candidates = [candidates_best_scores[0].key, candidates_best_scores[1].key]
+    ticker_probabilities = get_company_probabilities(candidates)
+    # preparing entities for response
+    source = Entity(scores=candidates_best_scores[0], ticker=ticker_probabilities[0])
+    target = Entity(scores=candidates_best_scores[1], ticker=ticker_probabilities[1]) 
+    prob = min(candidates_best_scores[0].score * ticker_probabilities[0].probability, candidates_best_scores[1].score * ticker_probabilities[1].probability)  
+    # if tickers match (Case 3)  
+    if ticker_probabilities[0].ticker == ticker_probabilities[1].ticker:
+        # return here and avoid one level of nesting
+        return PMatch(
+            probability=prob,
+            match_case=MatchType.TICKER,
+            source=source,
+            target=target
+            )
+    # if not, let's proceed with the other cases
+    
+    # override case: no match, but entities are very close to each other
+    prob_ee = calculate_probability_ee(clean_org(e1), clean_org(e2))
+    if prob_ee > theta_ee:
+        # this is case 1 again
+        return PMatch(
+            probability=prob_ee,
+            match_case=MatchType.MAYBE,
+            source=source,
+            target=target
+            )
 
-    if key_score_tuple_e1[0] == key_score_tuple_e2[0]:
-        # matching key case (Case 2)
-        final_probability = min(key_score_tuple_e1[1], key_score_tuple_e2[1])
-        match = 2
-        logger.info(f'Case 2: Keys match with probability {final_probability}')
-    else:
-        # get tickers corresponding to keys
-        ticker_probability_tuple1, ticker_probability_tuple2 = get_candidate_company_probability(key_score_tuple_e1,
-                                                                                                 key_score_tuple_e2)
-        logger.info(
-            f'Key {key_score_tuple_e1[0]}' + \
-            f' is mapped to {ticker_probability_tuple1[0]}' + \
-            f' with probability {ticker_probability_tuple1[1]}')
-        logger.info(
-            f'Key {key_score_tuple_e2[0]}' + \
-            f' is mapped to {ticker_probability_tuple2[0]}' + \
-            f' with probability {ticker_probability_tuple2[1]}')
-        final_probability = min(key_score_tuple_e1[1] * ticker_probability_tuple2[1],
-                  key_score_tuple_e2[1] * ticker_probability_tuple2[1])
-
-        dct_out[e1]['ticker'] = ticker_probability_tuple1[0]
-        dct_out[e2]['ticker'] = ticker_probability_tuple2[0]
-        dct_out[e1]['ticker_probability'] = ticker_probability_tuple1[1]
-        dct_out[e2]['ticker_probability'] = ticker_probability_tuple2[1]
-
-        if ticker_probability_tuple1[0]==ticker_probability_tuple2[0]:
-            # tickers match (Case 3)
-            match = 3
-            logger.info(f'Case 3: Keys do not match, but tickers match with probability {final_probability}')
-        else:
-            # tickers don't match (Case 4)
-            match = 4
-            logger.info(f'Case 4: Keys and tickers do not match with probability {final_probability}')
-
-    if match == 4:
-        # override case: no match, but entities are very close to each other
-        prob_ee = calculate_probability_ee(e1_clean, e2_clean)
-        if prob_ee > theta_ee:
-            match = 1
-            final_probability = prob_ee
-            logger.info(f'Case 1: Direct entity comparison overrider with probability {final_probability}')
-
-    if match == 4:
-        logger.info(f'*****No Match: Case {match} with probability is {final_probability}')
-    else:
-        logger.info(f'*****Match: Case {match} with probability is {final_probability}')
-
-    dct_out['probability'] = final_probability
-    dct_out['match_case'] = match
-
-    return dct_out
+    # if nothing else matched, we return this
+    return PMatch(
+            probability=prob,
+            match_case=MatchType.FOO,
+            source=source,
+            target=target
+            )
