0f0adc4c14124414518966fae62bc8b01afa7d95
apo <tagliabue.jacopo@gmail.com>
2022-01-17 09:10:14 -0500

    some comments in main

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ import logging
 import json
 from matching_algo import calculate_probability_match
 
+
+# cool - I never used a logger in lambda since printing goes directly to CloudWatch (lazy me!)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 streamHandler = logging.StreamHandler()
 logger.addHandler(streamHandler)
 
 
+# should probably call the function in a more perspicous way, e..g. like the endpoint ;-)
 def lambda_handler(event, context):
+    """
+    
+    Some comments here, in whatever format you want
+
+    """
 
     logger.info(f"Event Info: {event}")
     logger.info(f"Context Info: {context}")
 
     q = event['queryStringParameters']
+    # maybe return a "request not valid" if somebody skips the params?
+    # or, is there value anyway in running the algo without BOTH entities specified?
     e1 = q.get('e1', 'null1')
     e2 = q.get('e2', 'null2')
 
